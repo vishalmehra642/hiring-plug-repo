e76a90bcbc7bde3f29583e89957f024576cee2bd
vishalmehra642 <pkkumar642@gmail.com>
2022-01-27 19:25:32 +0530

    changes

--- src/components/Candidates/Candidates_Childs/CandidatesLayout.js
+++ src/components/Candidates/Candidates_Childs/CandidatesLayout.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch, faFilter } from "@fortawesome/free-solid-svg-icons";
-import Board from 'react-trello'
-import {data} from '../config'
+
 
 function CandidatesLayout() {
   return (
